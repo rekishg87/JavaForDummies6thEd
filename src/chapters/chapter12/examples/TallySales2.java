3ea29946a850aebd6850ece84563ed51026c05b4
Rekish <rekishg87@gmail.com>
2015-08-17 16:25:20 +0200

    First commit
    First commit

---
+++ src/chapters/chapter12/examples/TallySales2.java
@@ -0,0 +1,18 @@
+package chapters.chapter12.examples;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+/**
+ * Created by Rekish on 8/17/2015.
+ */
+public class TallySales2 {
+
+    public static void main(String args[]) {
+        ArrayList<Sale> sales = new ArrayList<Sale>();
+        NumberFormat currency =
+                NumberFormat.getCurrencyInstance();
+
+        //fillTheList(sales);
+    }
+}
