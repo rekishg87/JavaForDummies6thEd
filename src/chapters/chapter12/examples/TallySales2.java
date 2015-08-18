51182922a9d622456eab16fca4f86fd3f38570d8
Rekish <rekishg87@gmail.com>
2015-08-18 09:07:38 +0200

    Completed TellySales2

--- src/chapters/chapter12/examples/TallySales2.java
+++ src/chapters/chapter12/examples/TallySales2.java
@@ -13,6 +13,19 @@ public class TallySales2 {
         NumberFormat currency =
                 NumberFormat.getCurrencyInstance();
 
-        //fillTheList(sales);
+        fillTheList(sales);
+        System.out.println(currency.format
+                (sales.stream()
+                .filter((sale) -> sale.item.equals("DVD"))
+                .map((sale) -> sale.price)
+                .reduce
+                        (0.0, (price1, price2) -> price1 + price2)));
+    }
+
+    static void fillTheList(ArrayList<Sale> sales) {
+        sales.add(new Sale("DVD", 15.00));
+        sales.add(new Sale("Book", 12.00));
+        sales.add(new Sale("DVD", 21.00));
+        sales.add(new Sale("CD", 5.25));
     }
 }
